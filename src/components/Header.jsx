c99162a4b26c00c31f38edb58dfe9b338e564ce7
jhonatan182 <jona.vand98@gmail.com>
2022-02-12 11:02:58 -0600

    un poco

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
     return ( 
         
         <h1 className="font-black text-5xl text-center md:w-2/3 mx-auto">
-            Seguimiento Pacientes {''} con gitHub {''}
+            Seguimiento Pacientes {''} con gitHub a ver que pasa {''}
             <span className="text-indigo-600">Veterinaria</span>
         </h1>
         
